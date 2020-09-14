1686d92d4f22389d74d7410702b8e4dee6fdc2a6
Jordynvt <65914755+Jordynvt@users.noreply.github.com>
2020-09-14 16:36:15 +0200

    Added Enemy Class

---
+++ Enemy Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hero_And_Goblin_Game_V_3
+{
+    abstract class Enemy_Class : Charachter_Class
+    {
+        protected Random obj;
+
+        public Enemy_Class(int X, int Y, int enemyDamage, int maxHp, int startHp, char symbol)
+        {
+            return;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+    }
+}
