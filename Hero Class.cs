fd324ca5887370b9c1c4cca1285932343ac16142
Jordynvt <65914755+Jordynvt@users.noreply.github.com>
2020-09-14 16:45:44 +0200

    Added Hero Class

---
+++ Hero Class.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hero_And_Goblin_Game_V_3
+{
+    abstract class Hero_Class
+    {
+        public Hero(int x, int y, int HP) : this(Character(x, y)) //DID THIS
+        {
+            int HeroDamage = 2;
+        }
+        public override ReturnMove()
+        {
+            return
+            else
+            {
+                MovementEnum.None;
+            }
+        }
+
+        public override string ToString()
+        {
+            //Player Stats:
+            //HP: HP / Max HP
+            //Damage: 2
+            //[X, Y]
+        }
+    }
+}
