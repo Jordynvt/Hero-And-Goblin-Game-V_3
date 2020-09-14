d6b0e35e1a39c8606d617813d256c86532c7e50c
Jordynvt <65914755+Jordynvt@users.noreply.github.com>
2020-09-14 16:38:19 +0200

    Added goblin Class

---
+++ Goblin Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hero_And_Goblin_Game_V_3
+{
+    abstract class Goblin
+    {
+        public Goblin(int X, int Y)                   //Where I Started 
+        {
+            int GoblinHP = 10;
+            int GoblinDamage = 1;
+        }
+        public override MovementEnum ReturnMove(MovementEnum move = Character.MovementEnum.No_Movement)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
